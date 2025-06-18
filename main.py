271bcdb433c75f7e260c49c73bad3c74be807e96
Aneesh-9 <158072753+Aneesh-9@users.noreply.github.com>
2025-06-18 21:21:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-# main.py
-import os
-import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
-def home():
-    return {"message": "Virtual TA is running successfully!"}
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+def read_root():
+    return {"message": "TDS Virtual TA is live!"}
